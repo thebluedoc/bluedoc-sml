57c643903f798a1dc44d37f1307636d260a6fd6d
Jason Lee <huacnlee@gmail.com>
2019-01-11 12:57:16 +0800

    Do not raise error event not expect format

--- test/sml_test.rb
+++ test/sml_test.rb
@@ -4,17 +4,23 @@ require "test_helper"
 
 class BookLab::SML::Test < ActiveSupport::TestCase
   test "base to_html" do
-    ml = %(["html", { lang: "en" }, ["body", ["p", "Hello world"]]])
+    sml = %(["html", { lang: "en" }, ["body", ["p", "Hello world"]]])
     html = %(<html><body><p>Hello world</p></body></html>)
-    assert_equal html, BookLab::SML.parse(ml)
+    assert_equal html, BookLab::SML.parse(sml)
   end
 
   test "complex" do
-    ml = read_file("sample.sml")
-    html = BookLab::SML.parse(ml)
+    sml = read_file("sample.sml")
+    html = BookLab::SML.parse(sml)
 
     # puts format_html(html)
 
     assert_html_equal read_file("sample.html"), html
+  end
+
+  test "with invalid format" do
+    sml = %(["span",{"t":1},["span",{"t":0},"pre/post - "]])
+    html = BookLab::SML.parse(sml)
+    assert_equal "pre/post - ", html
   end
 end
