2d2f69a606ecd8ee61fc07124649d42dc54d0f82
Jason Lee <huacnlee@gmail.com>
2019-02-21 11:00:41 +0800

    Fix lint

--- lib/bluedoc/plantuml.rb
+++ lib/bluedoc/plantuml.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "zlib"
 
 module BlueDoc
@@ -7,7 +9,7 @@ module BlueDoc
         result = ""
         compressedData = Zlib::Deflate.deflate(text)
         compressedData.chars.each_slice(3) do |bytes|
-          #print bytes[0], ' ' , bytes[1] , ' ' , bytes[2]
+          # print bytes[0], ' ' , bytes[1] , ' ' , bytes[2]
           b1 = bytes[0].nil? ? 0 : (bytes[0].ord & 0xFF)
           b2 = bytes[1].nil? ? 0 : (bytes[1].ord & 0xFF)
           b3 = bytes[2].nil? ? 0 : (bytes[2].ord & 0xFF)
@@ -21,7 +23,7 @@ module BlueDoc
         c2 = ((b1 & 0x3) << 4) | (b2 >> 4)
         c3 = ((b2 & 0xF) << 2) | (b3 >> 6)
         c4 = b3 & 0x3F
-        return encode6bit(c1 & 0x3F).chr +
+        encode6bit(c1 & 0x3F).chr +
                encode6bit(c2 & 0x3F).chr +
                encode6bit(c3 & 0x3F).chr +
                encode6bit(c4 & 0x3F).chr
@@ -29,24 +31,24 @@ module BlueDoc
 
       def encode6bit(b)
         if b < 10
-          return ('0'.ord + b).chr
+          return ("0".ord + b).chr
         end
         b = b - 10
         if b < 26
-          return ('A'.ord + b).chr
+          return ("A".ord + b).chr
         end
         b = b - 26
         if b < 26
-          return ('a'.ord + b).chr
+          return ("a".ord + b).chr
         end
         b = b - 26
         if b == 0
-          return '-'
+          return "-"
         end
         if b == 1
-          return '_'
+          return "_"
         end
-        return '?'
+        "?"
       end
     end
   end
